c28b1b24382438acf37d75c6e447c4faacd9b329
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2019-03-14 17:45:16 -0300

    Update LogIn.java

--- src/test/java/com/page/LogIn.java
+++ src/test/java/com/page/LogIn.java
@@ -12,7 +12,7 @@ public class LogIn extends BasePage {
     private By newBtn = By.id("com.google.android.gm:id/welcome_tour_got_it");
     private By emails = By.id("com.google.android.gm:id/account_address");
     private By acessar = By.id("com.google.android.gm:id/action_done");
-    private By title = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TextView");
+    private By title = By.xpath("//android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TextView");
 
     @Step("Entrar no Aplicativo do Google Gmail.")
     public void openGmail() throws InterruptedException {
